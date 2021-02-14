26b8e4c8a137db3a4645607f586bceec603f7d07
Apdo3939 <apdo3939@hotmail.com>
2021-02-14 13:02:01 -0300

    Finishid the project

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -11,14 +11,16 @@ const Search = () => {
     const [search, setSearch] = useState('');
     const [urlSearch, setUrlSearch] = useState('');
 
+    const [resultEnableDiv, setResultEnableDiv] = useState(false);
+
     const onChangeSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearch(event.target.value);
     }
 
     const onClickSearch = () => {
+        setResultEnableDiv(true);
         setUrlSearch(`https://api.github.com/users/${search}`);
     }
-
 
     useEffect(() => {
         setIsLoading(true);
@@ -40,7 +42,6 @@ const Search = () => {
                             onChange={onChangeSearch}
                             className="search-input"
                         />
-
                         <button
                             value={urlSearch}
                             type="button"
@@ -52,32 +53,35 @@ const Search = () => {
 
                 </div>
             </div>
-            <div className="result-container">
-                <div className="result-content">
-                    {isLoading ?
-                        <div className="result-content-loader"><ImageLoader /> <InfoLoader /></div>
-                        :
-                        <div className="result-content-response" >
-                            <div>
-                                <img src={gitResponse?.avatar_url} alt={gitResponse?.login} className="result-card-image" />
-                            </div>
-                            <div>
-                                <div className="result-text-response-first">
-                                    <h2 className="result-text-response-h3">Repositórios Publicos:{gitResponse?.public_repos}</h2>
-                                    <h2 className="result-text-response-h3 resul-text-margin">Seguidores:{gitResponse?.followers}</h2>
-                                    <h2 className="result-text-response-h3 resul-text-margin">Seguindo:{gitResponse?.following}</h2>
+            {resultEnableDiv ?
+                <div className="result-container">
+                    <div className="result-content">
+                        {isLoading ?
+                            <div className="result-content-loader"><ImageLoader /> <InfoLoader /></div>
+                            :
+                            <div className="result-content-response" >
+                                <div>
+                                    <img src={gitResponse?.avatar_url} alt={gitResponse?.login} className="result-card-image" />
+                                    <a href={`https://github.com/${search}`} target="_blank" rel="noreferrer" className="search-button search-profile"> Ver perfil </a>
                                 </div>
-                                <div className="result-text-response">
-                                    <h1 className="result-text-title"> Informações</h1>
-                                    <h2 className="result-text-response-h2"> <strong>Empresa:</strong> {gitResponse?.company}</h2>
-                                    <h2 className="result-text-response-h2"><strong>Website/Blog:</strong>{gitResponse?.blog}</h2>
-                                    <h2 className="result-text-response-h2"><strong>Localidade:</strong>{gitResponse?.location}</h2>
-                                    <h2 className="result-text-response-h2"><strong>Membro desde:</strong>{gitResponse?.created_at}</h2>
+
+                                <div>
+                                    <div className="result-text-response-first">
+                                        <h2 className="result-text-response-h3">Repositórios Publicos:{gitResponse?.public_repos}</h2>
+                                        <h2 className="result-text-response-h3 resul-text-margin">Seguidores:{gitResponse?.followers}</h2>
+                                        <h2 className="result-text-response-h3 resul-text-margin">Seguindo:{gitResponse?.following}</h2>
+                                    </div>
+                                    <div className="result-text-response">
+                                        <h1 className="result-text-title"> Informações</h1>
+                                        <h2 className="result-text-response-h2"> <strong>Empresa:</strong> {gitResponse?.company}</h2>
+                                        <h2 className="result-text-response-h2"><strong>Website/Blog:</strong>{gitResponse?.blog}</h2>
+                                        <h2 className="result-text-response-h2"><strong>Localidade:</strong>{gitResponse?.location}</h2>
+                                        <h2 className="result-text-response-h2"><strong>Membro desde:</strong>{gitResponse?.created_at}</h2>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>}
-                </div>
-            </div>
+                            </div>}
+                    </div>
+                </div> : <div></div>}
 
         </>
     );
